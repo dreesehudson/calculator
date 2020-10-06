bfd679b628243f08b3dad86823254d357b15860c
mgaren01 <garen1989@gmail.com>
2020-10-06 16:45:53 -0400

    created pseudocode, index file, bones of model in js

---
+++ scratch.js
@@ -0,0 +1,13 @@
+var model 
+
+    //create empty array
+    let array = []
+
+    //concatenate number values until operator occurs, then break
+
+    //count # of values in array
+    for (i = 0; i <= array.length; i++){
+        if (i = 4) {
+            //perform equation
+        } 
+    }
